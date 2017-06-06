532cb6a7a224403466afec3985124e11dbc1c85a
Alex Law <agent_law@hotmail.com>
2017-06-06 11:19:15 +0100

    Add component App and Render

---
+++ index.ios.js
@@ -0,0 +1,15 @@
+// Index.ios.js - place code in here for IOS!!!
+
+// Import a library to help create a comoponent
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a Component
+const App = () => {
+  return (
+    <Text>Blah blah blah</Text>
+  );
+};
+
+// Render it to the device
+ ReactNative.AppRegistry.registerComponent('albums', () => App);
